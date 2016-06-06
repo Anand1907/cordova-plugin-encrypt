7ad331b70e2640b1b47df1f92c2fff7c00c33088
Anand1907 <anandmoorthy90@gmail.com>
2016-06-06 14:45:59 +0530

    Update CDVEncrypt.m

--- src/ios/CDVEncrypt.m
+++ src/ios/CDVEncrypt.m
@@ -9,21 +9,43 @@ MIT
 #import <Cordova/CDV.h>
 #import "CDVEncrypt.h"
 
+@implementation UIDevice (ModelVersion)
+
+- (NSString*)modelVersion
+{
+    size_t size;
+    
+    sysctlbyname("hw.machine", NULL, &size, NULL, 0);
+    char* machine = malloc(size);
+    sysctlbyname("hw.machine", machine, &size, NULL, 0);
+    NSString* platform = [NSString stringWithUTF8String:machine];
+    free(machine);
+    
+    return platform;
+}
+
+@end
 
 @interface CDVEncrypt () {}
 @end
 
 @implementation CDVEncrypt
 
+- (void)getKeyDetails:(CDVInvokedUrlCommand*)command
+{
+    NSDictionary* deviceProperties = [self deviceProperties];
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:deviceProperties];
+    
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
 - (NSDictionary*)deviceProperties
 {
-    UIDevice* device = [UIDevice currentDevice];
     NSMutableDictionary* devProps = [NSMutableDictionary dictionaryWithCapacity:4];
-
-    [devProps setObject:@"iOS" forKey:@"platform"];
+    
+    [devProps setObject:@"iOS" forKey:@"getKey"];
     NSDictionary* devReturn = [NSDictionary dictionaryWithDictionary:devProps];
     return devReturn;
 }
-
 
 @end
