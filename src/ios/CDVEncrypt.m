33a6be3a49ba7b2ecda6b7d6266eee8ef5141477
Anand1907 <anandmoorthy90@gmail.com>
2016-06-06 14:47:57 +0530

    Update CDVEncrypt.m

--- src/ios/CDVEncrypt.m
+++ src/ios/CDVEncrypt.m
@@ -1,5 +1,5 @@
 /*
-MIT
+ MIT
  */
 
 #include <sys/types.h>
@@ -8,23 +8,6 @@ MIT
 
 #import <Cordova/CDV.h>
 #import "CDVEncrypt.h"
-
-@implementation UIDevice (ModelVersion)
-
-- (NSString*)modelVersion
-{
-    size_t size;
-    
-    sysctlbyname("hw.machine", NULL, &size, NULL, 0);
-    char* machine = malloc(size);
-    sysctlbyname("hw.machine", machine, &size, NULL, 0);
-    NSString* platform = [NSString stringWithUTF8String:machine];
-    free(machine);
-    
-    return platform;
-}
-
-@end
 
 @interface CDVEncrypt () {}
 @end
